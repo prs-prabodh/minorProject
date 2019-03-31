5aac93ee51553a71fc893950566ce162a7f385a9
prs-prabodh <prabodhranjan98@gmail.com>
2019-04-01 01:06:52 +0530

    Added basic model training and prediction features

---
+++ model/model.py
@@ -0,0 +1,12 @@
+from sklearn.neighbors import KNeighborsClassifier
+
+def trainModel(data, label):
+    model = KNeighborsClassifier(n_neighbors=k, algorithm='auto')
+    model.fit(data, label)
+
+    return model
+
+def getPrediction(data):
+    prediction = model.predict(data)
+
+    return prediction
