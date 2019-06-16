2f17405ce40161b10c86ac241ba6c7037e6b8ca3
Prabodh Ranjan Swain <prabodhranjan98@gmail.com>
2019-06-16 14:33:03 +0530

    Improved accuracy using KNN

--- model/Training/KMeans_training.py
+++ model/Training/KMeans_training.py
@@ -2,21 +2,23 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.cluster import KMeans
+from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 
 data = pd.read_csv("extractedData.csv", index_col=False)
 train, test = train_test_split(data, test_size=0.2)
 
-x = train.drop(['Label'], axis=1)
-y = train['Label']
-y = (y == 1)
+x = np.array(train.drop(['Label'], axis=1))
+y = np.array(train['Label'])
+
+# Classify all types of attacks into one superclass ATTACK
+y = (y >= 1)
 
 X = np.array(test.drop(['Label'], axis=1))
 Y = np.array(test['Label'])
-Y = (Y == 1)
 
-x = np.array(x)
-y = np.array(y)
+# Classify all types of attacks into one superclass ATTACK
+Y = (Y >= 1)
 
 normaliser = MinMaxScaler()
 x = normaliser.fit_transform(x)
@@ -25,12 +27,20 @@ X = normaliser.fit_transform(X)
 kmeans = KMeans(n_clusters=2)
 kmeans.fit(x)
 
-correct = 0
+knn = KNeighborsClassifier(n_neighbors=10)
+knn.fit(x, y)
+
+correctKmeans = 0
+correctKnn = 0
 for i in range(len(X)):
     params = np.array(X[i])
     params = params. reshape(-1, len(params))
     prediction = kmeans.predict(params)
     if prediction[0] == Y[i]:
-        correct += 1
+        correctKmeans += 1
+    prediction = knn.predict(params)
+    if prediction[0] == Y[i]:
+        correctKnn += 1
 
-print(correct/len(X))
+print("KMeans Accuracy: ", correctKmeans/len(X))
+print("KNN Accuracy: ", correctKnn/len(X))
