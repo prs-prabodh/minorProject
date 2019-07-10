6ed571f0cde6a85573f7f914d4c1088a3d8e0674
Prabodh Ranjan Swain <prabodhranjan98@gmail.com>
2019-07-10 15:33:16 +0530

    Trained model

--- model/model.py
+++ model/model.py
@@ -1,4 +1,8 @@
 from sklearn.neighbors import KNeighborsClassifier
+import joblib as jl
+
+trained_model = jl.load('trained_model.pkl')
+
 
 def trainModel(data, label):
     model = KNeighborsClassifier(n_neighbors=k, algorithm='auto')
@@ -6,12 +10,15 @@ def trainModel(data, label):
 
     return model
 
+
 def getPrediction(data):
-    prediction = model.predict(data)
+    prediction = trained_model.predict(data)
 
     return prediction
 
-def analyze(packet):
-    # analyze packet
 
-    return True, 'None', 'Nil'
+def analyze(packet_data):
+    # analyze packet
+    result = getPrediction(packet_data)
+
+    return result == 0
